b4406652cef644b610ca76c6c77ea4440b76f618
Michael Calkins <m.calkins576@gmail.com>
2019-11-19 16:46:25 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,41 @@
 class Owner
-  # code goes here
+  attr_reader :name, :species
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @@all << self
+  end
+  
+  def say_species
+    p "I am a #{@species}."
+  end
+  
+  def cats 
+    Cat.all.select {|x| x.owner == self}
+  end
+  
+  def dogs
+    Dog.all.select {|x| x.owner == self}
+  end
+  
+  def buy_cat(cat_name)
+    cat_money = Cat.all.find {|x| x.name == cat_name}
+    cat_money.owner = self
+  end
+  
+  def self.all 
+    @@all 
+  end
+  
+  def self.count
+    @@all.length 
+  end
+  
+  def self.reset_all
+    @@all.clear
+  end
+  
 end
